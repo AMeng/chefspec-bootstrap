77b80e4f5005f90ce781b893abb9cb31ee16f17a
ameng <alexbmeng@gmail.com>
2014-12-23 15:56:13 -0500

    Test generated spec files with ChefSpec

--- spec/chefspec_bootstrap_spec.rb
+++ spec/chefspec_bootstrap_spec.rb
@@ -1,14 +1,17 @@
-require 'chefspec'
 require_relative '../lib/chefspec_bootstrap'
 require_relative 'spec_helper'
 
 describe 'Bootstrap' do
-  Dir.entries('spec/cookbooks/spec/recipes').select { |d| d.end_with?('.rb') }.each do |recipe|
-    it "creates the expected spec file for #{recipe}" do
-      bootstrap = ChefSpec::Bootstrap.new("spec/cookbooks/spec/recipes/#{recipe}", nil, nil, nil, nil)
-      template = File.open("spec/templates/#{recipe}", 'rb').read
+  recipes = Dir.entries('spec/cookbooks/spec/recipes').map do |file|
+    file.scan(/(.*).rb/).last.first if file.end_with?('.rb')
+  end.compact
 
-      expect { bootstrap.generate }.to output(template).to_stdout
+  recipes.each do |recipe|
+    it "creates the expected spec file for #{recipe}" do
+      bootstrap = ChefSpec::Bootstrap.new("spec/cookbooks/spec/recipes/#{recipe}.rb", nil, nil, nil, nil)
+      spec = File.open("spec/meta/#{recipe}_spec.rb", 'rb').read
+
+      expect { bootstrap.generate }.to output(spec).to_stdout
     end
   end
 end
