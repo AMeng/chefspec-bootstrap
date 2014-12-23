729fdfc48902c8b10bccc53539aadeddaa04f2e1
ameng <alexbmeng@gmail.com>
2014-12-23 13:14:34 -0500

    Fixed missing argument

--- spec/chefspec_bootstrap_spec.rb
+++ spec/chefspec_bootstrap_spec.rb
@@ -4,7 +4,7 @@ require_relative 'spec_helper'
 describe 'Bootstrap' do
   %w(failure directory package).each do |resource|
     it "creates the expected spec file for #{resource}" do
-      bootstrap = ChefSpec::Bootstrap.new("spec/cookbooks/spec/recipes/#{resource}.rb", nil, nil, nil)
+      bootstrap = ChefSpec::Bootstrap.new("spec/cookbooks/spec/recipes/#{resource}.rb", nil, nil, nil, nil)
       template = File.open("spec/templates/#{resource}.txt", 'rb').read
 
       expect { bootstrap.generate }.to output(template).to_stdout
